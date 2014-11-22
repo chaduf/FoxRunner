1d58ef6227cd20b61bd392272519e68830aa6e99
Maxioum <drumsticks@live.fr>
2014-11-22 17:58:49 +0100

    RotationFox

---
+++ Assets/Scripts/PlatformRL.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformRL : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	OnCollisionEnter(Collision col)
+	{
+
+	}
+}
